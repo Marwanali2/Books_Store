cdc8e38260376370319be86d5c4fb761c78ef7b4
marwan ali <marwanalizz42@gmail.com>
2023-04-12 23:59:51 +0200

    adding text animation in splach screen

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,4 +1,4 @@
 class AssetsData{
-  static const logo="https://img.freepik.com/free-icon/reading_318-927192.jpg?size=626&ext=jpg&uid=R51305451&ga=GA1.1.281577450.1681245494&semt=sph";
+  static const logo="assets/images/BOOKLY-logo.png";
 }
 //https://www.freepik.com/free-icon/book_15611380.htm#query=book&position=43&from_view=search&track=sph
--- lib/features/splach/presentation/views/widgets/splach_view_body.dart
+++ lib/features/splach/presentation/views/widgets/splach_view_body.dart
@@ -1,21 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:my_bookly_app/features/splach/presentation/views/widgets/sliding_text.dart';
 
 import '../../../../../core/utils/assets.dart';
-class SplachViewBody extends StatelessWidget {
+
+class SplachViewBody extends StatefulWidget {
   const SplachViewBody({Key? key}) : super(key: key);
 
   @override
+  State<SplachViewBody> createState() => _SplachViewBodyState();
+}
+
+class _SplachViewBodyState extends State<SplachViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        seconds: 3,
+      ),
+    );
+    slidingAnimation=Tween<Offset>(begin: const Offset(-1,0),end:Offset.zero).animate(animationController);
+   animationController.forward();
+  }
+
+  @override
+  void dispose() { // بنعملها في نهاية كل كنترولر علشان متهدرش الميموري و توقف تنفيذ الكنترولر اللي بنستخدمه مرة واحده
+    // TODO: implement dispose
+    super.dispose();
+    animationController.dispose();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Image.network(AssetsData.logo,color: Colors.orange,width: 100,height: 100,),
-          const SizedBox(height: 10,),
-          const Text('Read Free Books',textAlign: TextAlign.center,),
+          Image.asset(
+            AssetsData.logo,
+            color: Colors.green,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          SlidingText(slidingAnimation: slidingAnimation),
         ],
       ),
     );
   }
 }
+
+
