7b02a5c8122fb3d0584502448429005f146c4835
marwan ali <marwanalizz42@gmail.com>
2023-05-01 03:30:17 +0300

    create either class in home repo from dartz package

---
+++ lib/core/errors/failure.dart
@@ -0,0 +1,2 @@
+abstract class Failure {} // بنحط انواع الايرور كلها اللي ممكن تحصل في حالة استدعاء البيانات
+// class ServerFailure extends Failure{}
--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,4 +1,13 @@
-abstract class HomeRepo{ // بحط الميثودس اللي هتتنفذ في صفحة الهوم
-  fetchFeaturedBooks();
+import '../../../../core/errors/failure.dart';
+import '../models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  // بحط الميثودس اللي هتتنفذ في صفحة الهوم
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks(); // either is from package "dartz" use to handle error
+  // يعني هنا لما بستدعي البيانات فيه احتمالين الاول انها توصلي بالسلامة , الاحنمال التاني ان يحصل ايرور و متوصلش سواء ايرور بسبب السيرفر او مشكلة نت و هكذا
+  // فمفروض اهندل الاحتمالين دول عن طريق Either الموجودة في باكدج dart z
+  // انواع الايرور دول متجمعين في abstract class اسمه Failures
+
   fetchBestSellerBooks();
 }
