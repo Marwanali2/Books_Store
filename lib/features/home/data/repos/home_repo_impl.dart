2360cfddecd909037ff4bd4997a19957508dbdfc
marwan ali <marwanalizz42@gmail.com>
2023-05-03 12:34:05 +0300

    Create Server failure from dio error

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -2,28 +2,39 @@ import 'package:dartz/dartz.dart';
 import 'package:my_bookly_app/core/errors/failure.dart';
 import 'package:my_bookly_app/features/home/data/models/book_model/book_model.dart';
 import 'package:my_bookly_app/features/home/data/repos/home_repo.dart';
-
+import 'package:dio/dio.dart';
 import '../../../../core/utils/api_services.dart';
 
-class HomeRepoImpl implements HomeRepo { // هنا بنفذ ال methods اللي بتتنفذ في صفحة ال home
+class HomeRepoImpl implements HomeRepo {
+  // هنا بنفذ ال methods اللي بتتنفذ في صفحة ال home
   final ApiServices apiServices;
 
   HomeRepoImpl(this.apiServices);
 
   @override
   Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
-   try {
-     var newestBooksMapData=await apiServices.get(
+    try {
+      Map<String, dynamic> newestBooksMapData = await apiServices.get(
           endPoint:
               "volumes?Filtering=free-ebooks&q=subject:Programming&Sorting=newest");
-     List<BookModel>newestBooksList=[];
-     for(var item in newestBooksMapData['items']){
-       newestBooksList.add(BookModel.fromJson(item));
-     }
-     return right(newestBooksList);
-   } on Exception catch (e) {
-     return left(ServerFailure());
-   }
+      List<BookModel> newestBooksList = [];
+      for (var item in newestBooksMapData['items']) {
+        newestBooksList.add(BookModel.fromJson(item));
+      }
+      return right(newestBooksList);
+    } on Exception catch (e) {
+      if (e is DioError) {
+        return Left(
+          ServerFailure.fromDioError(e),
+        );
+      } else {
+        return left(
+          ServerFailure(
+            errorMessage: e.toString(),
+          ),
+        );
+      }
+    }
   }
 
   @override
